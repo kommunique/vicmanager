61fc4804f930e247ba3a602e48f9b20030b25dff
aurimas113 <aurimas113@gmail.com>
2022-08-04 11:23:34 +0300

    Initial backend

---
+++ backend/gamemanager/gamemanager/urls.py
@@ -0,0 +1,12 @@
+"""
+Definition of urls for gamemanager.
+"""
+
+from datetime import datetime
+from django.urls import path
+from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+urlpatterns = [
+]
